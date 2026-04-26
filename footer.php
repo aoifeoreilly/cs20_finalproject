b8ca312c5bc4770f2462251b20f961a4f0786116
aoifeoreilly <aoifeoreilly2022@gmail.com>
2026-04-26 18:21:48 -0400

    footer needs to have links to all pages

--- footer.php
+++ footer.php
@@ -2,8 +2,10 @@
     <footer>
         <p>
         &copy; <?php echo date('Y'); ?> <strong>TuftsEats</strong>&nbsp;&nbsp;
-        <a href="contact.php">Contact Us</a>&nbsp;&nbsp;
+        <a href="home.php">Recipe Builder</a>&nbsp;&nbsp;
+        <a href="contact.php">Contact</a>&nbsp;&nbsp;
         <a href="food_reviews.php">Reviews</a>
+        <a href="menus.php">Tufts Menus</a>
         </p>
     </footer>
 
